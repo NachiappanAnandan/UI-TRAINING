ff6b4a2305b50e1e775fdb69bb367990c8306de9
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-03-16 18:20:08 +0530

    Exercise11

---
+++ Calculator_Ex11/script.js
@@ -0,0 +1,24 @@
+var Calculator= {
+    num1:0,
+    num2:0,
+    add:function(){
+        return this.num1+this.num2
+    },
+    subtract:function(){
+        return this.num1-this.num2
+    },
+    multiply:function(){
+        return this.num1*this.num2
+    },
+    divide:function(){
+        return this.num1/this.num2
+    }
+}
+
+Calculator.num1= 90
+Calculator.num2= 10
+
+console.log(Calculator.num1 +" + "+Calculator.num2+ " = "+Calculator.add());
+console.log(Calculator.num1 +" - "+Calculator.num2+ " = "+Calculator.subtract());
+console.log(Calculator.num1 +" * "+Calculator.num2+ " = "+Calculator.multiply());
+console.log(Calculator.num1 +" / "+Calculator.num2+ " = "+Calculator.divide());
