a5a38dfcbe9796a2a5fc07f9a8e38229c142a1b1
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-03-17 10:04:00 +0530

    Review Comments changes

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var Calculator= {
+   add:(num1 , num2) => num1+num2,
+   subtract:(num1 , num2) => num1-num2,
+   multiply:(num1 , num2) => num1*num2,
+   divide:(num1 , num2) => (num1/num2).toFixed(2)
+}
+
+num1 = parseInt(window.prompt("Enter value for number 1"))
+num2 = parseInt(window.prompt("Enter value for number 2"))
+
+
+console.log(num1 +" + "+num2+ " = "+Calculator.add(num1 , num2));
+console.log(num1 +" - "+num2+ " = "+Calculator.subtract(num1 , num2));
+console.log(num1 +" * "+num2+ " = "+Calculator.multiply(num1 , num2));
+console.log(num1 +" / "+num2+ " = "+Calculator.divide(num1 , num2));
