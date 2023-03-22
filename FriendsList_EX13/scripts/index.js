e4c5d2fec142ea8d8c190c170f69104f5e05fd2f
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-03-22 23:51:08 +0530

    changes

--- FriendsList_EX13/scripts/index.js
+++ FriendsList_EX13/scripts/index.js
@@ -1,9 +1,14 @@
-
-var element = "";
+//fragment
+let fragment = document.createDocumentFragment();
 fetch("./scripts/friends.json")
     .then(response => response.json())
         .then(data => {
             for(let i of data){
+                // card element
+                let card = document.createElement("div");
+                card.className = "card";
+                
+
                 element += '<div class="card">'
                 element+='<div class="card_img"><img src="'+i["img"]+'" alt="profile image" ></div>'
                 element+='<div class="details"><div class="name">'+i['first_name']+" "+ i["last_name"]+'</div><div class="email">'+i["email"]+'</div></div></div>'    
