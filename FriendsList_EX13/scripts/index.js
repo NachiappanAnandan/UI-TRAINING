951aa319dc254e3c43772dc6182de8e0f0b30c3f
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-03-23 01:02:27 +0530

    changes

--- FriendsList_EX13/scripts/index.js
+++ FriendsList_EX13/scripts/index.js
@@ -1,5 +1,6 @@
 //fragment
 let fragment = document.createDocumentFragment();
+
 fetch("./scripts/friends.json")
     .then(response => response.json())
         .then(data => {
@@ -7,12 +8,32 @@ fetch("./scripts/friends.json")
                 // card element
                 let card = document.createElement("div");
                 card.className = "card";
-                
 
-                element += '<div class="card">'
-                element+='<div class="card_img"><img src="'+i["img"]+'" alt="profile image" ></div>'
-                element+='<div class="details"><div class="name">'+i['first_name']+" "+ i["last_name"]+'</div><div class="email">'+i["email"]+'</div></div></div>'    
+                // card image 
+                let img = document.createElement("img");
+                img.src = i["img"];
+                img.alt = "profile image";
+                // card image holder
+                let cardImg = document.createElement("div");
+                cardImg.className = "card-img";
+                cardImg.appendChild(img);
+                card.appendChild(cardImg);
+                //name and email 
+                let name = document.createElement("div");
+                name.className = "name";
+                name.textContent = i["first_name"]+" "+i["last_name"];
+                let email = document.createElement("div");
+                email.className = 'email';
+                email.textContent = i["email"];
+                // details
+                let details =document.createElement("div");
+                details.className = "details";
+                details.appendChild(name);
+                details.appendChild(email);
+                card.appendChild(details)
+                //append in fragment 
+                fragment.appendChild(card);
             }
-            document.getElementById("parent").innerHTML = element;
+            document.getElementById("parent").appendChild(fragment);
             }
         );
