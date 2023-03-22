47278088726eb7166976fdd5ad253edd4c84c2a8
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-03-23 01:18:16 +0530

    changes

--- FormVdlidators_Ex14/scripts/index.js
+++ FormVdlidators_Ex14/scripts/index.js
@@ -5,11 +5,11 @@ const regex = {
     firstname:/^[a-zA-Z]{1,30}$/,
     lastname:/^[[a-zA-Z]{1,30}$/,
     email:/^\w+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/g,
-    contact:/^[0-9]{10}$/,
+    contact:/^(\+91)[0-9]{10}$/,
     pincode:/^[0-9]{6}$/,
     cardNumber:/^[0-9]{16}$/,
     cardExpiry:/^202[3-9]$|^[2-9][0-9][3-9][0-9]$/,
-    ccv:/^[0-9]{3,4}$/
+    cvv:/^[0-9]{3,4}$/
 
 }
 
@@ -17,12 +17,12 @@ const complete =() => {
     form.querySelectorAll("input").forEach(element => {
        if(element.value == ""){
         element.classList.add("colorChange")
-        element.nextElementSibling.textContent =element.name +" is required.";
+        element.nextElementSibling.textContent =element.name +" is required";
         element.nextElementSibling.style.display = "block";      
        }else if(!regex[element.id].test(element.value)){ 
         element.classList.add("colorChange")
         element.nextElementSibling.style.display = "block";
-       element.nextElementSibling.innerHTML =element.name +" is not valid.";
+       element.nextElementSibling.innerHTML =element.name +" is not valid";
        }else{
          element.classList.remove("colorChange");
           element.nextElementSibling.style.display = "none";      
