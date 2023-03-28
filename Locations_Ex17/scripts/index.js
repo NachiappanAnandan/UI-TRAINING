066b6b6327dda7d305d3ac9a3e596e4fd7745ddf
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-03-28 19:07:16 +0530

    changes

--- Locations_Ex17/scripts/index.js
+++ Locations_Ex17/scripts/index.js
@@ -15,28 +15,33 @@ fetch("./scripts/locations.json")
     .then(response => response.json())
         .then(data => tableAdd(data))
 
+const createRow = (data) => {
+    const row = document.createElement("tr");
+    Object.keys(data).forEach(i=>{
+        let cell = document.createElement("td");
+        if(i =="country"){
+            const imgHolder = document.createElement("div")
+            imgHolder.className = "flagHolder"
+            const img = document.createElement("img");
+            img.src = "./images/"+data[i]+".png";
+            imgHolder.append(img);
+            cell.appendChild(imgHolder);
+        }else{
+            cell.textContent = data[i];
+        }
+        row.appendChild(cell);
+       
+       
+    })
+    return row;
+}
+
 const tableAdd = (data) => {
-    let fragments = document.createDocumentFragment();
+    const fragments = document.createDocumentFragment();
     data.forEach(element => {
-        console.log(element);
-        let row = document.createElement("tr");
-        Object.keys(element).forEach(i=>{
-            console.log(i);
-            let cell = document.createElement("td");
-            if(i =="country"){
-                let imgHolder = document.createElement("div")
-                imgHolder.className = "flagHolder"
-                let img = document.createElement("img");
-                img.src = "./images/"+element[i]+".png";
-                imgHolder.append(img);
-                cell.appendChild(imgHolder);
-            }else{
-                cell.textContent = element[i];
-            }
-           row.appendChild(cell);
-           
-        })
-        fragments.appendChild(row);
+        // console.log(element);
+        console.log();
+        fragments.appendChild(createRow(element));
     });
     document.getElementById("table").append(fragments);
 
